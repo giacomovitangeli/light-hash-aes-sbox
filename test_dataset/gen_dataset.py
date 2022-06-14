52bd86598df393a537e3db0472d06fefed0c0acd
Alessandro <a.niccolini@protonmail.com>
2022-06-14 11:20:19 +0200

    update hash algorithm

--- test_dataset/gen_dataset.py
+++ test_dataset/gen_dataset.py
@@ -9,11 +9,15 @@ def ISHFTC(n, d, N):
 
 H = [int('34',16), int('55',16), int('0F',16), int('14',16), int('DA',16), int('C0',16), int('2B',16), int('EE',16)]
 
-print(int('55',16))
-print("---------------------------------------------------------------")
-prova = 'M'.encode('utf-8')
-hex_of_str = prova.hex()
-int_str = int(hex_of_str, 16)
+prova = 'abcdefghijklmnopqrstuvwxyz'.encode('utf-8')
+print("Messaggio: " + prova.decode('utf-8'))
+'''h = 'e'.encode('utf-8').hex()
+int_i = int(h, 16)
+print(int_i)
+print("--------------------")
+print(prova[1])
+print("--------------------")'''
+#int_str = int(hex_of_str, 16)
 #print(hex_of_str)
 #print(codecs.decode('OF', 'hex_codec'))
 #print(aes_sbox.lookup(0))
@@ -21,25 +25,30 @@ int_str = int(hex_of_str, 16)
 #print(aes_sbox.print_aes_sbox_serialization(100))
 
 #print(xor(77, 15)) # 42
-#for j in range
-for r in range(0, 32):
-        for i in range(0, 8):
-            print("H mi da: " + str(H[(i+2) % 8]))
-            xor_res = xor(H[(i+2) % 8], int_str)
-            print(xor_res)
-            #index = xor_res << i
-            index = ISHFTC(xor_res, i, 8)
-            print("indice: " + str(index))
-            print(aes_sbox.lookup(index))
-            H[i] = aes_sbox.lookup(index)
-            print("salvo in H: " + str(H[i]))
+l = len(prova)
+for j in range(0, l):
+    int_str = prova[j]
+    for r in range(0, 32):
+            for i in range(0, 8):
+                #print("H mi da: " + str(H[(i+2) % 8]))
+                xor_res = xor(H[(i+2) % 8], int_str)
+                #print(xor_res)
+                #index = xor_res << i
+                index = ISHFTC(xor_res, i, 8)
+                #print("indice: " + str(index))
+                #print(aes_sbox.lookup(index))
+                H[i] = aes_sbox.lookup(index)
+                #print("salvo in H: " + str(H[i]))
 
 
 result = ""
+result_int = ""
 for i in range(0,8):
+    result_int += str(H[i])
     result += hex(H[i]).split('x')[-1]
 
-print(result)
+print("hash: " + result)
+print("hash_int: " + result_int)
 """with open('dataset.txt', 'w') as f:
     for r in range(0, 32):
         for i in range(0, 8):

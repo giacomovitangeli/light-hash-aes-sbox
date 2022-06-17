54e12ad0351d1d521b1a967f8e5a4b7e962fe181
Alessandro <a.niccolini@protonmail.com>
2022-06-17 15:31:39 +0200

    finish mainfor

--- project_930II_21.22_light_hash_algorithm_aes_sbox/db/LH_AES.sv
+++ project_930II_21.22_light_hash_algorithm_aes_sbox/db/LH_AES.sv
@@ -6,7 +6,7 @@
 module light_hash (
 	input            clk	//1-bit clock
 	,input            rst_n
-	,input		[7:0] ptxt_char	//8-bit input 
+	,input		[7:0] ptxt_char	//8-bit input
 	,input			ptxt_valid
 	,output reg [63:0] digest_char	//64-bit digest output --> composition of 8 blocks H[i] of 8-bit
 	,output reg		digest_ready
@@ -20,7 +20,7 @@ module light_hash (
 	localparam UPPERCASE_A_CHAR = 8'h41;  // ASCII code for 'A'
 	localparam UPPERCASE_Z_CHAR = 8'h5A;  // ASCII code for 'Z'
 	localparam LOWERCASE_A_CHAR = 8'h61;  // ASCII code for 'a'
-	localparam LOWERCASE_Z_CHAR = 8'h7A;  // ASCII code for 'z'	
+	localparam LOWERCASE_Z_CHAR = 8'h7A;  // ASCII code for 'z'
 	localparam LOWERBOUND_0_CHAR = 8'h30;  // ASCII code for '0'
 	localparam UPPERBOUND_9_CHAR = 8'h39;  // ASCII code for '9'
 
@@ -28,12 +28,13 @@ module light_hash (
 	wire       ptxt_char_is_lowercase_letter;
 	wire	   ptxt_char_is_number;
 	wire       ptxt_char_is_letter;
-	
+
 	//assign digest_char = `NULL_CHAR;	//TODO verify this assignment
-	
+
 
 	reg [7:0] digest_tmp[0:7];	//64-bit temporary digest
-	
+	// reg to make the circular left shift
+	reg [7:0] shifter;
 
 	// ---------------------------------------------------------------------------
 	// Logic Design
@@ -41,18 +42,18 @@ module light_hash (
 
 	assign ptxt_char_is_uppercase_letter = (ptxt_char >= UPPERCASE_A_CHAR) &&
                                          (ptxt_char <= UPPERCASE_Z_CHAR);
-                                         
+
 	assign ptxt_char_is_lowercase_letter = (ptxt_char >= LOWERCASE_A_CHAR) &&
                                          (ptxt_char <= LOWERCASE_Z_CHAR);
-                                         
+
 	assign ptxt_char_is_letter = ptxt_char_is_uppercase_letter ||
                                ptxt_char_is_lowercase_letter;
-							   
+
 	assign ptxt_char_is_number = (ptxt_char >= LOWERBOUND_0_CHAR) &&
 								(ptxt_char <= UPPERBOUND_9_CHAR);
 
 	// assign err_invalid_ptxt_char = !ptxt_char_is_letter or !ptxt_char_is_number;
-	wire err_invalid_ptxt_char_wire = (!ptxt_char_is_letter) && 
+	wire err_invalid_ptxt_char_wire = (!ptxt_char_is_letter) &&
 									(!ptxt_char_is_number);
 
 	always @ (posedge clk or negedge rst_n)
@@ -66,12 +67,18 @@ module light_hash (
 	always @ (*) begin
 	//check plaintext validity
 	if(ptxt_valid) begin
-		for(int r = 0; r < 32; r++) begin
+		gfor(int r = 0; r < 32; r++) begin
 			for(int i = 0; i < 8; i++) begin
-				for(int j=0; j < 8; j++) begin
-					digest_tmp[i][j] = aes128_sbox((digest_tmp[(i+2) % 8] ^ ptxt_char) << i);
-					$display("%b", digest_tmp[i]);
-				end				
+				digest_tmp[i] = (digest_tmp[(i+2) % 8] ^ ptxt_char);
+				shifter = update_digest[j];
+				digest_tmp[i] = shift_digest(shifter, i);
+				// 4 MSb and the 4 LSb of input byte as row and column of sbox lut to substitute it
+        		row = digest_tmp[i][7:4];
+        		column = digest_tmp[i][3:0];
+        		index = (row * 16) + column;
+				$display("%b", index);
+				digest_tmp[i] = aes128_sbox(index);
+				$display("%b", digest_tmp[i]);
 			end
 		end
 	end
@@ -79,12 +86,14 @@ module light_hash (
 
 	// Output char (ciphertext)
 	always @ (posedge clk or negedge rst_n)
-	if(!rst_n)
-	  digest_char <= `NULL_CHAR;
+	if(!rst_n) begin
+		digest_char <= `NULL_CHAR;
+  	    digest_tmp <= restore_digest();
+	end
 	else if(err_invalid_ptxt_char_wire)
 	  digest_char <= `NULL_CHAR;
 	else
-	  digest_char <= digest_tmp;
+	  digest_char <= get_digest(digest_tmp);
 
 
 endmodule
--- project_930II_21.22_light_hash_algorithm_aes_sbox/db/LH_functions.sv
+++ project_930II_21.22_light_hash_algorithm_aes_sbox/db/LH_functions.sv
@@ -9,3 +9,29 @@ function reg [7:0] digest_tmp [0:7] restore_digest;
 	restore_digest[6] = 8'h2B;
 	restore_digest[7] = 8'hEE;
 endfunction
+
+// copy the digest to the output
+function [63:0] get_digest(input [7:0] digest[0:7]);
+	get_digest[7:0] = digest[7];
+	get_digest[15:8] = digest[6];
+	get_digest[23:16] = digest[5];
+	get_digest[31:24] = digest[4];
+	get_digest[39:32] = digest[3];
+	get_digest[47:40] = digest[2];
+	get_digest[55:48] = digest[1];
+	get_digest[63:56] = digest[0];
+endfunction
+
+// hard left circular shift
+function reg [7:0] shift_digest(input [7:0] shifter, input [3:0] j);
+	case (j)
+		0 : shift_digest = shifter;
+		1 : shift_digest = {shifter[6:0], shifter[7]};
+		2 : shift_digest = {shifter[5:0], shifter[7:6]};
+		3 : shift_digest = {shifter[4:0], shifter[7:5]};
+		4 : shift_digest = {shifter[3:0], shifter[7:4]};
+		5 : shift_digest = {shifter[2:0], shifter[7:3]};
+		6 : shift_digest = {shifter[1:0], shifter[7:2]};
+		7 : shift_digest = {shifter[0:0], shifter[7:1]};
+	endcase
+endfunction
